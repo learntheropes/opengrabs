05baf95ee4fdfe93bf2312c4cac926b5f1e597b9
Giovanni La Perna <giovannilaperna@gmail.com>
2022-01-24 19:31:47 +0100

    admin sidebar links to tawk.to

--- components/admin/side-bar.vue
+++ components/admin/side-bar.vue
@@ -4,6 +4,17 @@
         <li><nuxt-link :to="localePath({ name: 'index' })">Website homepage</nuxt-link></li>
         <li><nuxt-link to="/admin">Admin homepage</nuxt-link></li>
     </ul>
+    <p class="menu-label">Tawk.to</p>
+    <ul class="menu-list">
+        <li><a href="https://dashboard.tawk.to" target="_blank">Dashboard</a></li>
+    </ul>
+    <p class="menu-label">Knowledge Base</p>
+    <ul class="menu-list">
+        <li><a href="https://opengrabs.tawk.help/" target="_blank">English</a></li>
+        <li><a href="https://opengrabs.tawk.help/es" target="_blank">Spanish</a></li>
+        <li><a href="https://opengrabs.tawk.help/pt" target="_blank">Portuguese</a></li>
+        <li><a href="https://opengrabs.tawk.help/ru" target="_blank">Russina</a></li>
+    </ul>
     <p class="menu-label">Grabs</p>
     <ul class="menu-list">
         <li><nuxt-link to="/admin/grabs">Grabs</nuxt-link></li>
