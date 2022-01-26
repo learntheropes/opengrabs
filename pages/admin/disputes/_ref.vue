c025940d2996220a526f2815f0357cbc003ed41a
Giovanni La Perna <giovannilaperna@gmail.com>
2022-01-26 16:19:07 +0100

    update page content

--- pages/admin/disputes/_ref.vue
+++ pages/admin/disputes/_ref.vue
@@ -12,8 +12,8 @@
                             <div class="content">
                                 <p>Buyer: {{ grab.buyer.name }}</p>
                                 <p>Traveler: {{ grab.traveler.name }}</p>
-                                <p>Product: <a :href="grab.shop.url" target="_blank">{{ grab.shop.slug.replaceAll('-','') || grab.shop.title }}</a></p>
-                                <p>Price: {{ grab.shop.price.total.toFix(2) }} {{ grab.shop.currency }}</p>
+                                <p v-if="grab.shop.slug">Product: <a :href="grab.shop.url" target="_blank">{{ grab.shop.slug.replaceAll('-','') || grab.shop.title }}</a></p>
+                                <p>Price: {{ grab.shop.price.total.toFixed(2) }} {{ grab.shop.currency }}</p>
                                 <p>Delivery date: {{ $moment(grab.delivery.date).fromNow() }} [{{ $utils.momentDate(grab.delivery.date) }}]</p>
                                 <p v-if="grab.published_at">Published {{ $moment(grab.published_at).fromNow() }}</p>
                                 <p v-if="grab.booked_at">Booked {{ $moment(grab.booked_at).fromNow() }}</p>
