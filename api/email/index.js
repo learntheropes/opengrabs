7d5be16c1eb5b83a0047d3b81bbe9d0c6ed17c50
Giovanni La Perna <giovannilaperna@gmail.com>
2022-01-30 15:55:44 +0100

    nodemailer

--- api/email/index.js
+++ api/email/index.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp.migadu.com",
     port: 465,
     secure: true,
     auth: {
       user: 'no-reply@opengrabs.com',
       pass: process.env.MIGADU_PASSWORD,
-    },
+    }
+  }, {
+    from: '"OpenGrabs" <no-reply@opengrabs.com>',
   })
--- api/routes/user.js
+++ api/routes/user.js
@@ -103,29 +103,11 @@ router.post('/db/user/update', authorizeUser, asyncHandler(async (req,res) => {
       emailContent = en.emailConfirmationCode(props.code)
   }
 
-  transporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-    }
-    else if (success) {
-      console.log('success')
-    }
-    else {
-      console.log("Server is ready to take our messages");
-    }
-  });
-
-  try {
-    const email = await transporter.sendMail({
-      from: '"OpenGrabs" <no-reply@opengrabs.com>',
-      to: props.email,
-      subject: emailContent.subject,
-      text: emailContent.content,
-    })
-    console.log(email);
-  } catch (error) {
-    console.log(error)
-  }
+  await transporter.sendMail({
+    to: props.email,
+    subject: emailContent.subject,
+    text: emailContent.content,
+  })
 
   res.status(200).json({})
 }))
