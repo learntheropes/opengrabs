97582660b12b4d64ead4e7d5e75552bfddeb97ae
Giovanni La Perna <giovannilaperna@gmail.com>
2022-02-02 08:01:52 +0100

    redirect to the previous fullPath at login page

--- pages/login.vue
+++ pages/login.vue
@@ -12,8 +12,10 @@ export default {
         return { fullPath: from.fullPath, path: from.path }
     },
     created() {
-        if (this.path !== '/cb' && this.path !== `${this.$i18n.locale}/orders` && this.path !== `${this.$i18n.locale}/travels`) this.$auth.$storage.setUniversal('redirect', this.fullPath)
-        this.$auth.loginWith('auth0')
+        if (this.path !== '/cb' && this.path !== `${this.$i18n.locale}/orders` && this.path !== `${this.$i18n.locale}/travels`) {
+            this.$auth.$storage.setUniversal('redirect', this.fullPath)
+            this.$auth.loginWith('auth0')
+        }
     }
 }
 </script>
