c56fa7c5c21ee1e86a9c178c7f294b83eaac2e91
Giovanni La Perna <giovannilaperna@gmail.com>
2022-02-15 10:57:00 +0100

    fix error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,8 @@
 <template>
-  <section class="hero is-fullheight">
-    <div class="hero-body hero-body-shopper">
+  <section class="hero is-fullheight-with-navbar">
+    <div class="hero-body">
       <div class="container has-text-centered">
-        <p v-if="error.statusCode === 404" class="title">Page not found</p>
-        <p v-else class="title">{{ error.message }}</p>
+        <p class="title">{{ error.message }}</p>
         <nuxt-link :to="localePath({ name: 'index' })" class="button is-primary">Home page</nuxt-link>
       </div>
     </div>
