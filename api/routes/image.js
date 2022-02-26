604c38a374710fc5f9b3b65da4b9e1e579cb0a44
Giovanni La Perna <giovannilaperna@gmail.com>
2022-02-26 14:49:22 +0100

    test page for imagekit bug report

--- api/routes/image.js
+++ api/routes/image.js
@@ -11,18 +11,22 @@ router.post('/image/signature', (req,res) => {
 
 const path = "/testnet/grabs/6f02cb4126aa1ae5a40504a836cb83ef_nB8g6vw-_.png"
 
-const getImageKitWorking = () => {
+const notWorkingTextWatermark = 'opengrabs.com'
 
-  const text = 'testnet.opengrabs.com'
+const getNotWorkingBase64Watermark = () => {
+  const text = notWorkingTextWatermark
   const buff = new Buffer.from(text)
   const base64text = buff.toString('base64')
+  return base64text
+}
 
+const getImageKitNotWorking = () => {
   return imagekit.url({
     path,
     transformation : [{
-      width: "800",
+      width: "400",
       ofo: "center",
-      ote: base64text,
+      ote: getNotWorkingBase64Watermark(),
       otc: "00000066",
       otbg: "62616066",
       or: "10"
@@ -32,18 +36,22 @@ const getImageKitWorking = () => {
   })
 }
 
-const getImageKitNotWorking = () => {
+const workingTextWatermark = 'testnet.opengrabs.com'
 
-  const text = 'opengrabs.com'
+const getWorkingBase64Watermark = () => {
+  const text = workingTextWatermark
   const buff = new Buffer.from(text)
   const base64text = buff.toString('base64')
+  return base64text
+}
 
+const getImageKitWorking = () => {
   return imagekit.url({
     path,
     transformation : [{
-      width: "800",
+      width: "400",
       ofo: "center",
-      ote: base64text,
+      ote: getWorkingBase64Watermark(),
       otc: "00000066",
       otbg: "62616066",
       or: "10"
@@ -57,9 +65,15 @@ router.get('/image/test', (req,res) => {
 
   res.set('Content-Type', 'text/html')
   res.send(Buffer.from(`
-<h1>Not Working: </h1><pre>` + getImageKitNotWorking() + `</pre><br>
-<img src="` + getImageKitNotWorking() + `" ><br>
-<h1>Working: </h1><pre>` + getImageKitWorking() + `</pre><br>
+<h1>Not Found</h1>
+<p>Not Found Watermark: </p><pre>` + notWorkingTextWatermark + `</pre></br>
+<p>Not Found base64 Watermark: </p><pre>` + getNotWorkingBase64Watermark() + `</pre></br>
+<p>Not Found Url: </p><pre>` + getImageKitNotWorking() + `</pre><br>
+<img src="` + getImageKitNotWorking() + `" ><br><br>
+<h1>Working</h1>
+<p>Working Watermark: </p><pre>` + workingTextWatermark + `</pre></br>
+<p>Working base64 Watermark: </p><pre>` + getWorkingBase64Watermark() + `</pre></br>
+<p>Working Url: </p><pre>` + getImageKitWorking() + `</pre><br>
 <img src="` + getImageKitWorking() + `" ><br>
 `))
 })
