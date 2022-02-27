d5d1668c7e1b9af29c950cd3a628c9247b8702d1
Giovanni La Perna <giovannilaperna@gmail.com>
2022-02-27 19:49:42 +0100

    seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,7 +64,7 @@ export default {
         {
           hid: 'og:description',
           property: 'og:description',
-          content: this.$t('description')
+          content: this.$t('seo.description')
         },
         {
           hid: 'og:type',
