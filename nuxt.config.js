97f2c2bc5b762acbc841c58aa0755bca9397fdde
Giovanni La Perna <giovannilaperna@gmail.com>
2022-02-28 11:57:27 +0100

    seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-headproducts
   head: {
-    titleTemplate: '%s - OpenGrabs',
+    titleTemplate: '%s | OpenGrabs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
